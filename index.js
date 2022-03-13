973286b82a4c672c208e43b634efeac14a976de4
unknown <amandavu@gmail.com>
2022-03-13 19:32:10 +0100

    add, edit, delete to do and to do list endpoints and db

--- index.js
+++ index.js
@@ -2,17 +2,249 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const {
+  getUserById,
+  getUserByEmail,
+  createUser,
+  editUser,
+  getToDosById,
+  getToDoLists,
+  createToDoList,
+  createToDo,
+  editToDo,
+  getToDoById,
+  deleteToDoById,
+  deleteToDoListById,
+} = require("./services/database");
 
 // Express Server
 
 const port = process.env.PORT;
+const secret = process.env.SECRET;
 
 const app = express();
 
 app.use(cors());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Server functions
+
 app.get("/", (req, res) => {
   res.send({ message: "Hello from Planner API!" });
+});
+
+app.get("/users/:userid", async (req, res) => {
+  try {
+    const userId = req.params.userid;
+    const user = await getUserById(userId);
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      error: "Unable to contact database - please try again",
+    });
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  const { firstname, surname, email, password, img } = req.body;
+  // Hashing password
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  try {
+    const newUser = await createUser(
+      firstname,
+      surname,
+      email,
+      hashedPassword,
+      img
+    );
+    res.send(newUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      error: "Unable to contact database - please try again",
+    });
+  }
+});
+
+app.put("/users/:userid", async (req, res) => {
+  const { id, firstname, surname, email, password, img } = req.body;
+
+  // checks if password is changed for re-hashing
+  const user = await getUserById(id);
+  const passwordIsChanged = user.password !== password;
+  let newPassword = "";
+
+  if (passwordIsChanged) {
+    const saltRounds = 10;
+    newPassword = await bcrypt.hash(password, saltRounds);
+  }
+
+  try {
+    const updatedUser = editUser(
+      id,
+      firstname,
+      surname,
+      email,
+      passwordIsChanged ? newPassword : password,
+      img
+    );
+
+    res.send(updatedUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      error: "Unable to contact database - please try again",
+    });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  try {
+    const user = await getUserByEmail(email);
+    console.log(user);
+    if (!user) {
+      return res.status(401).send({ error: "Unknown user" });
+    }
+    // Load hash from your password DB
+    const isCorrectPassword = await bcrypt.compare(password, user.password);
+    console.log(isCorrectPassword);
+    if (!isCorrectPassword) {
+      console.log("not correct password");
+      return res.status(401).send({ error: "Wrong password" });
+    } else {
+      const token = jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+          firstname: user.firstname,
+          surname: user.surname,
+        },
+        Buffer.from(secret, "base64")
+      );
+
+      console.log(token);
+      res.send({
+        token: token,
+      });
+    }
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+app.get("/todolists/:userId", async (req, res) => {
+  const toDoLists = await getToDoLists(req.params.userId);
+  res.status(200).send(toDoLists);
+});
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const todolistId = req.params.id;
+    const todos = await getToDosById(todolistId);
+    res.send(todos);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      error: "Unable to contact database - please try again",
+    });
+  }
+});
+
+app.post("/todolists", async (req, res) => {
+  const { name, owner, day } = req.body;
+
+  try {
+    const newToDoList = await createToDoList(
+      name,
+      new Date().toISOString(),
+      owner,
+      day
+    );
+    res.send(newToDoList);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      error: "Unable to contact database - please try again",
+    });
+  }
+});
+
+app.post("/todo", async (req, res) => {
+  const { text, startTime, checked, todolistId } = req.body;
+
+  try {
+    const newToDo = await createToDo(text, startTime, checked, todolistId);
+    res.send(newToDo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      error: "Unable to contact database - please try again",
+    });
+  }
+});
+
+app.put("/todo/:id", async (req, res) => {
+  const { id, text, startTime } = req.body;
+
+  try {
+    const newToDo = await editToDo(id, text, startTime);
+    res.send(newToDo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      error: "Unable to contact database - please try again",
+    });
+  }
+});
+
+app.get("/todo/:id", async (req, res) => {
+  try {
+    const todoId = req.params.id;
+    const todo = await getToDoById(todoId);
+    res.send(todo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      error: "Unable to contact database - please try again",
+    });
+  }
+});
+
+app.delete("/todo/:id", async (req, res) => {
+  try {
+    const todoId = req.params.id;
+    const todo = await deleteToDoById(todoId);
+    res.status(200).send(todo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      error: "Unable to contact database - please try again",
+    });
+  }
+});
+
+app.delete("/todolist/:id", async (req, res) => {
+  try {
+    const todolistId = req.params.id;
+    const todolist = await deleteToDoListById(todolistId);
+    res.status(200).send(todolist);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      error: "Unable to contact database - please try again",
+    });
+  }
 });
 
 var server = http.createServer(app);
---
+++ services/database.js
@@ -0,0 +1,191 @@
+const { Pool } = require("pg");
+const pgp = require("pg-promise")();
+
+const connection = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+};
+
+const db = pgp(connection);
+
+const database = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+function getUserById(id) {
+  return database
+    .query(
+      `
+      SELECT * FROM users WHERE id = $1
+    `,
+      [id]
+    )
+    .then((results) => results.rows[0]);
+}
+
+function getUserByEmail(email) {
+  return database
+    .query(
+      `
+    SELECT id, firstname, surname, email, password, img FROM users WHERE email = $1
+  `,
+      [email]
+    )
+    .then((results) => results.rows[0]);
+}
+
+function createUser(firstname, surname, email, password, img) {
+  return database
+    .query(
+      `
+    INSERT INTO users
+      (firstname, surname, email, password, img)
+    VALUES
+      ($1, $2, $3, $4, $5)
+    RETURNING
+      *
+  `,
+      [firstname, surname, email, password, img]
+    )
+    .then((results) => results.rows[0]);
+}
+
+function editUser(id, firstname, surname, email, password, img) {
+  return database
+    .query(
+      `UPDATE users SET (firstname, surname, email, password, img) = ($2, $3, $4, $5, $6)
+      WHERE id = $1
+      RETURNING
+      *
+      `,
+      [id, firstname, surname, email, password, img]
+    )
+    .then((results) => results.rows[0]);
+}
+
+function getToDoLists(userId) {
+  return database
+    .query(
+      `SELECT *
+    FROM todolist
+    WHERE owner = ${userId}`
+    )
+    .then((results) => results.rows);
+}
+
+function getToDosById(id) {
+  return database
+    .query(
+      `SELECT *
+    FROM todo
+    WHERE todolistid = ${id}`
+    )
+    .then((results) => results.rows);
+}
+
+function createToDoList(name, createdAt, owner, day) {
+  return database
+    .query(
+      `
+    INSERT INTO todolist
+      (name, createdAt, owner, day)
+    VALUES
+      ($1, $2, $3, $4)
+    RETURNING
+      *
+  `,
+      [name, createdAt, owner, day]
+    )
+    .then((results) => results.rows[0]);
+}
+
+function createToDo(text, startTime, checked, todolistId) {
+  return database
+    .query(
+      `
+    INSERT INTO todo
+      (text, starttime, checked, todolistid)
+    VALUES
+      ($1, $2, $3, $4)
+    RETURNING
+      *
+  `,
+      [text, startTime, checked, todolistId]
+    )
+    .then((results) => results.rows[0]);
+}
+
+function editToDo(id, text, startTime) {
+  return database
+    .query(
+      `UPDATE todo SET (text, starttime) = ($2, $3)
+      WHERE id = $1
+      RETURNING
+      *
+      `,
+      [id, text, startTime]
+    )
+    .then((results) => results.rows[0]);
+}
+
+function getToDoById(id) {
+  return database
+    .query(
+      `SELECT *
+    FROM todo
+    WHERE id = ${id}`
+    )
+    .then((results) => results.rows[0]);
+}
+
+function deleteToDoById(id) {
+  return database
+    .query(
+      `DELETE 
+    FROM todo
+    WHERE id = ${id}`
+    )
+    .then((results) => results.rows);
+}
+
+function deleteToDoListById(id) {
+  return database
+    .query(
+      `DELETE 
+    FROM todolist
+    WHERE id = ${id}`
+    )
+    .then((results) => results.rows);
+}
+
+// async function deleteUser(id) {
+//   console.log(id);
+//   const query = await db
+//     .multi(
+//       `
+//       DELETE FROM likes WHERE from_user_id = ${id} OR to_user_id = ${id};
+//       DELETE FROM messages WHERE from_user_id = ${id} OR to_user_id = ${id};
+//       DELETE from users WHERE id = ${id};
+//       `
+//     )
+//     .then((res) => {
+//       return res.rows;
+//     });
+//   return { userDeleted: true };
+// }
+
+module.exports = {
+  getUserById,
+  getUserByEmail,
+  createUser,
+  editUser,
+  getToDosById,
+  getToDoLists,
+  createToDoList,
+  createToDo,
+  editToDo,
+  getToDoById,
+  deleteToDoById,
+  deleteToDoListById,
+};
